8decc59cb55b89eeff1eed40a6ebee46b34c66d6
Leslie Ngo <ngo.leslie@gmail.com>
2022-12-05 11:49:22 -0800

    fn_invalidAction: Add helper fn actionValidator
    to provide an easy way to add or remove tests for validating the
    actions array in each entry, and to improve readability and
    organization.

--- validator.js
+++ validator.js
@@ -21,59 +21,63 @@ const invalidTopLevelKeys = (data) => {
   return false;
 }
 
+const actionValidator = (index, actionObj, date, timestampRegex) => {
+  const { ts, action, unit, quantity } = actionObj;
+  const keys = Object.keys(actionObj);
+
+  const testList = [
+    {
+      test: keys.every(key => ['ts', 'action', 'unit', 'quantity'].includes(key)) && keys.length === 4,
+      message: `Invalid keys in action property at index ${index}.`
+    },
+    {
+      test: timestampRegex.test(ts),
+      message: `Invalid timestamp at actions index ${index}.`
+    },
+    {
+      test: ts.slice(0, 10) === date,
+      message: 'Actions are not all on the same day.'
+    },
+    {
+      test: typeof action === 'string',
+      message: `Action is not of type string at actions index ${index}.`
+    },
+    {
+      test: ['mile', 'floor', 'minute', 'quantity'].includes(unit),
+      message: `Unit is not one of mile, floor, minute, or quantity at actions index ${index}.`
+    },
+    {
+      test: typeof quantity === 'number' && quantity !== NaN && quantity > 0,
+      message: `Invalid quantity at index ${index}. Must be number type positive integer.`
+    },
+  ]
+
+  for(const entry of testList) {
+    if(!entry.test) {
+      return entry.message;
+    }
+  }
+  return false;
+}
+
 const invalidActions = (actions) => {
   if(!actions) {
     return 'Actions array is empty.';
   }
   const timestampRegex = /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01]) ([01]?\d|2[0-3]):([0-5][0-9]):([0-5][0-9])$/
-
-  if(!actions[0].ts || typeof actions[0].ts !== 'string') {
-    return 'Date used to verify all actions performed on same day is invalid.';
+  const firstTimestamp = actions[0].ts;
+  if(!firstTimestamp || !timestampRegex.test(firstTimestamp)) {
+    return 'Timestamp from action index 0 is invalid.';
   }
-
   const date = actions[0].ts.slice(0, 10);
-  let errorHint = '';
 
   for(const [index, actionObj] of actions.entries()) {
-    const { ts, action, unit, quantity } = actionObj;
-    const keys = Object.keys(actionObj);
-
-    const keysAreValid = keys.every(key => ['ts', 'action', 'unit', 'quantity'].includes(key)) && keys.length === 4;
-    const dateIsValid = timestampRegex.test(ts);
-    const isSameDay = ts.slice(0, 10) === date;
-    const actionIsString = typeof action === 'string';
-    const unitIsValid = ['mile', 'floor', 'minute', 'quantity'].includes(unit);
-    const quantityIsNumber = typeof quantity === 'number' && quantity !== NaN && quantity > 0;
-
-    if(!keysAreValid) {
-      errorHint = `Invalid keys in action property at index ${index}.`
-      break;
-    }
-    if(!dateIsValid) {
-      errorHint = `Invalid timestamp at action index ${index}.`
-      break;
-    }
-    if(!isSameDay) {
-      errorHint = 'Actions are not all on the same day.'
-      break;
-    }
-    if(!actionIsString) {
-      errorHint = `Action is not of type string at index ${index}.`
-      break;
-    }
-    if(!unitIsValid) {
-      errorHint = `Unit is not one of mile, floor, minute, or quantity at index ${index}.`
-      break;
-    }
-    if(!quantityIsNumber) {
-      errorHint = `Invalid quantity at index ${index}. Must be number type positive integer.`
-      break;
+    const actionValidatorErrorMessage = actionValidator(index, actionObj, date, timestampRegex);
+    if(actionValidatorErrorMessage) {
+      return actionValidatorErrorMessage;
     }
   }
 
-  if(errorHint) {
-    return errorHint;
-  }
   return false;
 }
 
